b87a7cabc6eb9480cf7a836ab4cc9c9c8d950e6f
Edson h <edsony04@hotmail.com>
2021-05-02 07:53:30 -0300

    voltando bkp

---
+++ HFIT/HFIT/HFIT/Views/WorkoutDetails.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HFIT.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WorkoutDetails : ContentPage
+    {
+        public WorkoutDetails()
+        {
+            InitializeComponent();
+        }
+        private void GoBack(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
